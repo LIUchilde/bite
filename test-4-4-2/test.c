250ff6a981a86793c488df38c8908482a0e23bcc
LIUchilde <1961324638@qq.com>
2019-04-24 16:57:16 +0800

    添加代码

---
+++ test-4-4-2/test.c
@@ -0,0 +1,20 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int year; 
+	scanf("%d", &year);
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+	{
+		printf("%d是闰年", year);
+	}
+	else
+	{
+		printf("%d不是闰年", year);
+	}
+	system("pause");
+	return 0;
+}
+ 
