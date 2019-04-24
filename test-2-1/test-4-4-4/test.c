4e0f905fb4940697647c9807eb42409584c5add7
LIUchilde <1961324638@qq.com>
2019-04-24 17:00:22 +0800

    添加代码

---
+++ test-2-1/test-4-4-4/test.c
@@ -0,0 +1,18 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	//int t = 0;
+	printf("请输入两个数\n");
+	scanf("%d %d", &a, &b);
+	//t=a , a =b , b = t;
+	a = a + b; b = a - b; a = a - b;
+	printf("%d %d", a, b);
+	system("pause");
+	return 0;
+}
+ 
