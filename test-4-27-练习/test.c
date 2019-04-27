bfc5c342ab1fcd2316a2785d4c47d90388576a31
LIUchilde <1961324638@qq.com>
2019-04-27 23:44:07 +0800

    打印三角

---
+++ test-4-27-练习/test.c
@@ -0,0 +1,67 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	//int i = 0;
+	/*for (i = 0; i < 30; i++)
+	{
+		printf("*");
+
+	}*/
+	/*int i = 0;
+	int j = 0;
+	for (i = 0; i < 30; i++)
+	{
+		for (j = 0; j<30; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}*/
+	/*int i = 0;
+	int j = 0;
+	for (i = 0; i < 30; i++)
+	{
+		for (j = 0; j < 2 * i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}*/
+	//int i = 0;
+	//int j = 0;
+	//for (i = 0; i < 30; i++)
+	//{
+	//	for (j = 0; j < 30 - i; j++)
+	//	{
+	//		printf(" ");
+	//	}
+	//	for (j = 0; j < 2*i+1; j++)
+	//	{
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+	/*int i = 0;
+	int j = 0;
+	for (i = 30; i > 0; i--)
+	{
+		for (j =30-i ; j > 0; j--)
+		{
+			printf(" ");
+		}
+		for (j = 2 * i + 1; j > 0; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+
+	}
+*/
+
+	system("pause");
+	return 0;
+}
+ 
