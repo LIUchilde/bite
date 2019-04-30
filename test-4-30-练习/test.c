7b73c0af9e2ee9e406eebde273bc02fd4351a261
LIUchilde <1961324638@qq.com>
2019-04-30 23:13:57 +0800

    添加代码

---
+++ test-4-30-练习/test.c
@@ -0,0 +1,64 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int n= 0;
+//	scanf("%d %d", &i, &n);
+//	int s = 0;
+//	int j = i;
+//	while (n > 0)
+//	{
+//		s += i;
+//		i = i * 10 + j;
+//		n--;
+//	}
+//	printf("%d\n", s);
+//	system("pause");
+//	return 0;
+//}
+
+//一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在第10次落地时，共经过多少米？第10次反弹多高？
+ 
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	float s, h;
+//
+//	s =h= 100;
+//	h = h / 2;
+//	int i = 0;
+//	for (i = 2; i <= 10; i++)
+//	{
+//		s = s + 2 * h;
+//		h = h / 2;
+//	}
+//	printf("%f %f", s, h);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 1;
+//	int n = 0;
+//	for (n = 9; n > 0; n--)
+//	{
+//		j = (j + 1) * 2;
+//		i = j;
+//	}
+//	printf("%d", i);
+//	system("pause");
+//	return 0;
+//}
+
+
+ 
