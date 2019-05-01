ac8fbb61c80630c398a80697ee0f42c6a39c2328
LIUchilde <1961324638@qq.com>
2019-05-01 18:41:24 +0800

    添加代码

---
+++ test--5-1-练习/test.c
@@ -0,0 +1,106 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i, j;
+//	for (i = 0; i < 4; i++)
+//	{
+//		for (j = 0; j <= 2 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j <= 2 * i; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//			for (j = 0; j <= 4 - 2 * i; j++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
+//		system("pause");
+//		return 0;
+//	}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	float t = 0;
+//	float a = 2;
+//	float b = 1;
+//	float sum = 0;
+//	for (i = 0; i < 20; i++)
+//	{
+//		sum += a / b;
+//		t = a;
+//		a = a + b;
+//		b = t;
+//    }
+//	printf("%f\n", sum);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	long double sum = 0;
+//	 long double n = 1;
+//	for (i = 1; i <=20; i++)
+//	{
+//		n = n*i;
+//		sum = sum + n;
+//	}
+//	printf("%lf\n", sum);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int fun(int n)
+//{
+//	int x = 0;
+//	if (n < 2)
+//	{
+//		x = 1;
+//	}
+//	else
+//	{
+//		x = n*fun(n - 1);
+//	
+//	}
+//	return x;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入一个数\n");
+//	scanf("%d", &n);
+//	int ret=fun(n);
+//	printf("%d\n",ret);
+//	system("pause");
+//	return 0;
+//}
+
+
+ 
