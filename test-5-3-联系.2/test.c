46647c6377633317053814fb305ecf1f57ca68af
LIUchilde <1961324638@qq.com>
2019-05-03 20:53:42 +0800

    添加代码

---
+++ test-5-3-联系.2/test.c
@@ -0,0 +1,25 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+//逆置整数
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int fun(int n)
+//{
+//	if (n>0)
+//	{
+//		printf("%d", n % 10);
+//		fun(n / 10);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入一个整数\n");
+//	scanf("%d", &n);
+//	int ret = fun(n);
+//	system("pause");
+//	return 0;
+//}
+
