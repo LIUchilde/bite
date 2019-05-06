2ad8011af289137f56f611f8f3e0e9be3a713992
LIUchilde <1961324638@qq.com>
2019-05-06 11:31:33 +0800

    添加代码

---
+++ test-5-6-1/test.c
@@ -0,0 +1,54 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//void fun(char* s)
+//{
+//	if (*s != '\0')
+//	{
+//		*s++;
+//		fun(s);
+//		printf("%c", *(s - 1));
+//	}
+//
+//}
+//
+//int main()
+//{
+//	char s[10] = "0";
+//	printf("ÇëÊäÈëÒ»×é×Ö·û\n");
+//	scanf("%s", s);
+//	fun(s);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	for (n = 2; n <= 100; n++)
+//	{
+//		for (i = 2; i < n; i++)
+//		{
+//			if (n%i == 0)
+//			{
+//				break;
+//			}
+//
+//		}
+//		if (n == i)
+//		{
+//			printf("%d ", n);
+//		}
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+
+
+ 
