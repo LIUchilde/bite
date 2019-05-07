3b9fc6549502173577215f9ff98b3a35c3a833aa
LIUchilde <1961324638@qq.com>
2019-05-07 18:59:37 +0800

    添加代码

---
+++ test-5-7-2/test.c
@@ -0,0 +1,76 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//
+//int main()
+//{
+//	int arr[10];
+//	printf("请输入一组数\n");
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int t = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		t = arr[i];
+//		arr[i] = arr[9 - i];
+//		arr[9 - i] = t;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a=%d b=%d", a, b);
+//	printf("\n");
+//	a++;
+//	b++;
+//	printf("a=%d b=%d", a, b);
+//	printf("\n");
+//	a--;
+//	b--;
+//	printf("a=%d b=%d", a, b);
+//	printf("\n");
+//	++a;
+//	++b;
+//	printf("a=%d b=%d", a, b);
+//	printf("\n");
+//	--a;
+//	--b;
+//	printf("a=%d b=%d", a, b);
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int a, n, i, m, sum=0;
+//	scanf("%d %d", &a, &n);
+//	m = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		m = 10 * m + a;
+//		sum += m;
+//	}
+//	printf("%d", sum);
+//
+//	system("pause");
+//	return 0;
+//}
