41178037127547d418fd576a389c6c7c43c98405
LIUchilde <1961324638@qq.com>
2019-05-09 23:04:05 +0800

    1

---
+++ test-5-9-练习/test.c
@@ -0,0 +1,49 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i, j, sum;
+//	printf("请输入两个数\n");
+//	scanf("%d %d", &i, &j);
+//	sum = i + j;
+//	printf("%d", sum);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//
+//int main()
+//{
+//	int a, b, c, s, max, min;
+//	printf("请输入三个数\n");
+//	scanf("%d %d %d", &a, &b, &c);
+//	max = min = a;
+//	if (max < b)
+//	{
+//		max = b;
+//	}
+//	if (max < c)
+//	{
+//		max = c;
+//	}
+//	if (min>b)
+//	{
+//		min = b;
+//	}
+//	if (min > c)
+//	{
+//		min = c;
+//	}
+//	s = (a + b + c) / 3;
+//	printf("%d %d %d", max, min, s);
+//	system("pause");
+//	return 0;
+//}
+
+	
+ 
