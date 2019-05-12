0934e16347c26ed24c36bf16330de98ebff4ca75
LIUchilde <1961324638@qq.com>
2019-05-12 23:23:27 +0800

    添加代码

---
+++ test-5-12/test.c
@@ -0,0 +1,34 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//
+//int main()
+//{
+//	int len = strlen("abc");
+//	printf("%d\n", len);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int my_strlen(char* str)
+//{
+//	int count=0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int len = my_strlen("abc");
+//	printf("%d\n", len);
+//	system("pause");
+//	return 0;
+//}
+
