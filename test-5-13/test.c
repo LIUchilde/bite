10f80f12eb87d008ae7fe9ecb49faa0d3f12459d
LIUchilde <1961324638@qq.com>
2019-05-13 20:18:00 +0800

    添加代码

---
+++ test-5-13/test.c
@@ -0,0 +1,57 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int len = my_strlen("abc");
+//	printf("%d\n", len);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//#include<assert.h>
+//
+//char* my_strcpy(char* pdest, const char* psre)
+//{
+//
+//	
+//	char* start = pdest;
+//	assert(pdest != NULL);
+//	assert(psre != NULL);
+//	while (*pdest = *psre)
+//	{
+//
+//		pdest++;
+//		psre++;
+//	}
+//	return start;
+//}
+//
+//int main()
+//{
+//	char arr1[20];
+//	char arr2[] = "hello";
+//	printf("%s\n", strcpy(arr1, arr2));
+//	
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//
+//	system("pause");
+//	return 0;
+//}
+ 
