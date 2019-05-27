73b6de4aa9abdccbe8499d6852209c09e3a67555
LIUchilde <1961324638@qq.com>
2019-05-27 17:46:56 +0800

    添加代码

---
+++ test-5-27-1/源.c
@@ -0,0 +1,45 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	char* p = "hello word";
+//	printf("%s\n", p);
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	char str1[] = "hello word";
+//	char str2[] = "hello word";
+//	char* str3 = "hello word";
+//	char* str4 = "hello word";
+//	if (str1 == str2)
+//	{
+//		printf("str1 and str2 are same\n");
+//	}
+//	else
+//	{
+//		printf("str1 and str2 are not same\n");
+//	}
+//用相同的常量字符串去初始化不同的数组时，会开辟出不同的内存块，所以str1和str2不同。
+//	if (str3 == str4)
+//	{
+//		printf("str3 and str4 are same\n");
+//	}
+//	else
+//	{
+//		printf("str3 and str4 are not same\n");
+//	}
+//不同指针指向同一个字符串时，他们实际上会指向同一内存块，所以str3和str4相同
+//	system("pause");
+//	return 0;
+//}
+
+
+
