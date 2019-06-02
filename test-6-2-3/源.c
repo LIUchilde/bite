52217148e6bc210c13a7643cb225ca54908cdd44
LIUchilde <1961324638@qq.com>
2019-06-02 23:44:17 +0800

    添加代码

---
+++ test-6-2-3/源.c
@@ -0,0 +1,19 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//int main()
+//{
+//	char str[] = "- This,a sample string.";
+//	char* ph;
+//	ph = strtok(str, " ,.-");
+//	while (ph != NULL)
+//	{
+//		printf("%s\n", ph);
+//		ph = strtok(NULL, " ,.-");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
